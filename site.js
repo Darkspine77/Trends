e91eb7d1be671c8f243c3b0109e400af1a908314
Darkspine77 <Jwoods@Riverdale.edu>
2017-02-15 23:21:27 -0500

    added site files

---
+++ site.js
@@ -0,0 +1,61 @@
+function generateRGBA(){
+    r = parseInt(Math.random() * 255)
+    g = parseInt(Math.random() * 255)
+    b = parseInt(Math.random() * 255)
+    return 'rgba(' + r + ',' + g + ',' + b + ',1)';
+}
+
+var config = {
+            apiKey: "AIzaSyChY6_M8fYAwd2L3NkB9vXxsbFiqiCu6QQ",
+            authDomain: "trends-bd671.firebaseapp.com",
+            databaseURL: "https://trends-bd671.firebaseio.com",
+            storageBucket: "trends-bd671.appspot.com",
+            messagingSenderId: "783531718308"
+          };
+firebase.initializeApp(config);
+var database = firebase.database().ref();
+database.once('value').then(function(snapshot) {
+    var data = snapshot.val()
+    var namesList = [] 
+    var searchesList = []
+    var tweetsList = []
+    // var color1 = generateRGBA()
+    // var color2 = generateRGBA()
+    for (var i = data.length - 1; i >= 0; i--) {
+        namesList.push(data[i].name) 
+        searchesList.push(parseInt(data[i].amount.replace(",",""))/10000) 
+        tweetsList.push(data[i].tweets) 
+    }
+   var ctx = document.getElementById("myChart");
+   var data = {
+    labels: namesList,
+    datasets: [
+        {
+            label: "Searches (By 10,000)",
+            backgroundColor: "red",
+            data: searchesList
+        },
+        {
+            label: "Tweets",
+            backgroundColor: "blue",
+            data: tweetsList
+        }
+    ]
+};
+
+var myBarChart = new Chart(ctx, {
+    type: 'bar',
+    data: data,
+    options: {
+        barValueSpacing: 20,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    min: 0,
+                }
+            }]
+        }
+    }
+});
+});
+
